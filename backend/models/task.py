21d17a9804fbe5884e12c1656e0f07a4bdd18921
kosuke <kosuke@KosukenoMacBook-Air.local>
2023-11-14 00:24:25 +0900

    モデルのデータ型修正

--- backend/models/task.py
+++ backend/models/task.py
@@ -7,15 +7,15 @@ from sqlalchemy import Column, Integer, ForeignKey, String, Text, Boolean, Date,
 
 class TaskOrm(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True, nullable=False, index=True)
+    id = Column(Integer(), primary_key=True, nullable=False, index=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=True)
     title = Column(String(100), nullable=False)
     memo = Column(Text(1000), nullable=True)
-    start_date = Column(Date, nullable=False)
-    finish_date = Column(Date, nullable=False)
-    is_check = Column(Boolean, default=False, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+    start_date = Column(Date(), nullable=False)
+    finish_date = Column(Date(), nullable=False)
+    is_check = Column(Boolean(), default=False, nullable=False)
+    created_at = Column(DateTime(), default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime(), default=datetime.now(), onupdate=datetime.now(), nullable=False)
 
 class Task(BaseModel):
     id: int
--- backend/models/user.py
+++ backend/models/user.py
@@ -7,12 +7,12 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 class UserOrm(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, nullable=False, index=True)
+    id = Column(Integer(), primary_key=True, nullable=False, index=True)
     name = Column(String(100), nullable=False)
     email = Column(String(254), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
-    created_at = Column(DateTime, default=datetime.datetime.now(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now(), nullable=False)
+    created_at = Column(DateTime(), default=datetime.datetime.now(), nullable=False)
+    updated_at = Column(DateTime(), default=datetime.datetime.now(), onupdate=datetime.datetime.now(), nullable=False)
 
 class User(BaseModel):
     id: int
