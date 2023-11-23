c0f8aa590574afa9ebd34225a1d3d4fd8b8e6e56
kosuke <kosuke@KosukenoMacBook-Air.local>
2023-11-23 19:52:13 +0900

    詳細取得、更新処理、削除処理の実装

--- backend/handlers/task.py
+++ backend/handlers/task.py
@@ -11,9 +11,28 @@ logger = logging.getLogger(__name__)
 
 # タスクの新規作成
 @router.post("/new_task")
-def create_task(task: Task, user_repo: Annotated[TaskRepository, Depends(TaskRepository)]):
+def create_task(task: Task, task_repo: Annotated[TaskRepository, Depends(TaskRepository)]):
     try:
         with SessionLocal.begin() as db:
-            user_repo.create_task(db, task.title, task.memo, task.start_date, task.finish_date)
+            task_repo.create_task(db, task.title, task.memo, task.start_date, task.finish_date)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# タスクの詳細取得
+@router.get("/task/{id}", response_model=Task)
+def get_task(id: int, task_repo: Annotated[TaskRepository, Depends(TaskRepository)]):
+    try:
+        with SessionLocal() as db:
+            return task_repo.get_task_by_id(db, id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# タスクの更新処理
+@router.put("/task/update/{id}")
+def update_task(task: Task, task_repo: Annotated[TaskRepository, Depends(TaskRepository)]):
+    try:
+        with SessionLocal.begin() as db:
+            task_repo.update_task(db, task.id, task.title, task.memo, 
+                                    task.start_date, task.finish_date, task.is_check)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/repositories/task.py
+++ backend/repositories/task.py
@@ -20,3 +20,36 @@ class TaskRepository:
         )
         db.add(task)
         db.flush()
+    
+    # タスクの詳細取得
+    def get_task_by_id(self, db: Session, id: int) -> Task:
+        task = db.query(TaskOrm).filter(TaskOrm.id == id).first()
+        if task is None:
+            raise ValueError("Task is None")
+        
+        return task
+
+    # タスクの更新処理
+    def update_task(
+            self,
+            db: Session,
+            id: int,
+            title: str,
+            memo: str,
+            start_date: date,
+            finish_date: date,
+            is_check: bool
+        ) -> Task:
+
+        task = db.query(TaskOrm).filter(TaskOrm.id == id).first()
+
+        if task is None:
+            raise ValueError("Task is None")
+
+        task.title = title,
+        task.memo = memo,
+        task.start_date = start_date,
+        task.finish_date = finish_date,
+        task.is_check = is_check
+
+        db.flush()
