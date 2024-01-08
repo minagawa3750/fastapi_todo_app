7ae16cce5efbd970938740ab05057db4580e005c
kosuke <kosuke@KosukenoMacBook-Air.local>
2024-01-08 13:22:51 +0900

    ライブラリにaxios追加

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from settings.env import Env
 from handlers import task
 
 app = FastAPI()
+
+origins = [
+    Env.APP_URL,
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(task.router)
